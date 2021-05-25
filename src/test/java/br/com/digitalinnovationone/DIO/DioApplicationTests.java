d66060463c492a08dc77cd25efcf6c0df3fb91cd
Yasmin Carneiro <83085926+yasmincarneiro@users.noreply.github.com>
2021-05-25 00:24:04 -0300

    Primeiro projeto Dio

---
+++ src/test/java/br/com/digitalinnovationone/DIO/DioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovationone.DIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
